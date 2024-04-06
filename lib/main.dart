2b69f771df41fef99050abf19b5cbd6fbc267e08
Nigee Hettige <nigeehettige20@gmail.com>
2024-04-06 11:30:38 +0530

    modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:groupchar_flutter/pages/signin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const MyHomePage('Moda Chanuka'),
+      home: Signin()
     );
   }
 }
--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -11,8 +11,94 @@ class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:  Container(child: Column(children:[
-
-        ],),));
+        body: Container(
+      child: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                    colors: [Color(0xFF7f30fe), Color(0xFF6380fb)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight),
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.elliptical(
+                        MediaQuery.of(context).size.width, 105.0))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 70.0),
+            child: Column(
+              children: [
+                const Center(
+                  child: Text(
+                    'SignIn',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    'LogIn to your Account',
+                    style: TextStyle(
+                        color: Color(0xFFbbb0ff),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 20.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30.0, horizontal: 20.0),
+                      height: MediaQuery.of(context).size.height / 2,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Email",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            decoration: BoxDecoration(
+                              border:
+                                  Border.all(width: 1.0, color: Colors.black),
+                            ),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                prefixIcon: Icon(Icons.mail_outline,
+                                    color: Color(0xFF7f30fe)),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
